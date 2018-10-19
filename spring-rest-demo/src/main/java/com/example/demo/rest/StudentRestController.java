334e09a4bf3562f1a3b941a42ba8b3c8fddbaaf0
riskiidice <ampamp@ampamp.com>
2018-10-19 10:11:51 +0700

    Added @PathVariable

---
+++ spring-rest-demo/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -0,0 +1,45 @@
+package com.example.demo.rest;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+	
+	private List<Student> students  = new ArrayList<>();
+	
+	@PostConstruct
+	public void loadData(){	
+		
+		students.add(new Student("AMP","AMP"));
+		
+		students.add(new Student("Tikumporn","Wankvar"));
+		
+		students.add(new Student("Wankvar","Tikumporn"));
+
+		
+	}
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+		
+		return students;
+	}
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		return students.get(studentId);
+	}
+}
